bd681504fb5db3084d280de29e6b88dd9946d414
Syed Abdullah Shakir <abdullahshakir123@gmail.com>
2026-09-22 11:16:55 +0530

    Test auto deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Docker! Version 1\n");
+  res.end("Hello from Docker! Version 2\n");
 });
 
 server.listen(3000, "0.0.0.0", () => {
